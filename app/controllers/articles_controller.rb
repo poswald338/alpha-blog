28396b1f23cd43413387c325810c349c1e018187
Patrick Oswald <poswald338@gmail.com>
2021-11-19 14:36:27 -0600

    Adding create article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,14 @@ class ArticlesController < ApplicationController
     end
     def index 
         @articles = Article.all
-        
+    end
+    def new
+
+    end
+    def create 
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      @article.save
+      redirect_to article_path(@article)
+      #redirect_to articles_path()
     end
 end
