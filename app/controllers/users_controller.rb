7b4768d86cf17b5ddff10f9c167668f26294d38c
Patrick Oswald <poswald338@gmail.com>
2021-12-21 09:13:19 -0600

    add controller restrictions for user and destroy action for deleting user and blogs associated with that user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -30,6 +32,12 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all blogs successfully deleted"
+    redirect_to root_path
+  end
 
   private 
   def user_params
@@ -38,6 +46,13 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You are not authorized to perform that action"
+      redirect_to @user
+    end
   end
 
 end
