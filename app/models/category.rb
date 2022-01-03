99ee599708775f6403448e4bc70fae35c8548722
Patrick Oswald <poswald338@gmail.com>
2022-01-03 10:51:12 -0600

    add association for articles and categories from UI

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3, maximum: 20}
   validates_uniqueness_of :name, case_sensitive: false
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
